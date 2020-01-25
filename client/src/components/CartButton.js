be225d6ef56e7e9d4501776b900a709001a8a308
Thomas Mylott <mylottiv@gmail.com>
2020-01-25 17:33:02 -0600

    removed row wrapper for cart button

--- client/src/components/CartButton.js
+++ client/src/components/CartButton.js
@@ -3,13 +3,11 @@ import {Link} from 'react-router-dom';
 
 function CartButton(props) {
     return (
-        <div className='fixed flex flex-row-reverse inset-x-0 bottom-0'>
-            <Link to='/myCart'>
-                <p className='text-center bg-blue-600 rounded px-10 py-5 mr-5 align-bottom'>
-                    <img src='./shopping-cart.svg' /><span className='text-bold'>Cart</span>
-                </p>
-            </Link>
-        </div>
+        <Link to='/myCart'>
+            <p className='fixed bottom-0 right-0 text-center bg-blue-600 rounded px-10 py-5 mr-5 align-bottom'>
+                <img src='./shopping-cart.svg' /><span className='text-bold'>Cart</span>
+            </p>
+        </Link>
     )
 }
 
