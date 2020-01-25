6fd9eaf737ae4027d27a190efcf81db65b052332
Thomas Mylott <mylottiv@gmail.com>
2020-01-24 20:47:08 -0600

    Building out basic product grid with tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="text-green-800"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container mx-auto px-5'>
+      <div className='flex flex-row bg-red-700 my-1'>
+        <div className='flex flex-col w-full'>  
+          <div className='flex flex-row'>
+            <h1 className='flex-1 text-3xl font-bold text-center'>Build Your Own</h1>
+          </div>
+          <div className='flex flex-row'>
+            <div className="flex flex-col rounded bg-gray-200 px-1 py-1 m-1">
+              <div className="w-64 rounded overflow-hidden shadow-lg">
+                <img className="w-full h-48" src="https://i2.wp.com/lifemadesimplebakes.com/wp-content/uploads/2017/04/Garden-Veggie-Pizza-1.jpg" alt="Sunset in the mountains"></img>
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                  <p className="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                  </p>
+                </div>
+                <div className="px-6 py-4">
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                      Order
+                    </button>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-row bg-red-700 my-1'>
+        <div className="flex flex-col bg-gray-200 px-1 py-1 m-1">
+          <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+          <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+          <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+        </div>
+      </div>
+    </div> 
   );
 }
 
