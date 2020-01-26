9273c637b14baf96d5646a777ff346dbd0d56c1c
Thomas Mylott <mylottiv@gmail.com>
2020-01-26 17:23:56 -0600

    Cart quantity notification now a circle

--- client/src/components/shared/CartButton.js
+++ client/src/components/shared/CartButton.js
@@ -8,9 +8,15 @@ function CartButton(props) {
 
     return (
         <Link to='/myCart'>
-            <p className='fixed bottom-0 right-0 text-center bg-blue-600 rounded px-10 py-5 mr-5 align-bottom'>
-                <img src='./shopping-cart.svg' /><span className='text-bold'>Cart ({cartState.products.length})</span>
-            </p>
+            <div className='fixed flex flex-col bottom-0 right-0 text-center bg-blue-600 rounded px-12 py-5 mr-5 align-bottom'>
+                <img src='./shopping-cart.svg' />
+                <div className='flex flex-row font-bold'>
+                    <span className=''>Cart:</span>
+                    <span className='rounded-full h-6 w-6 flex flex-col items-center justify-center bg-red-700 ml-1 p-3'>
+                        {cartState.products.length}
+                    </span>
+                </div>
+            </div>
         </Link>
     )
 }
