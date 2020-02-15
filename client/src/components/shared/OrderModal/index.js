a32217c5e26c85b41bd05741be12a0dc72ee3e28
Thomas Mylott <mylottiv@gmail.com>
2020-02-15 13:08:27 -0600

    Starting to encapsulate OrderModal components

--- client/src/components/shared/OrderModal/index.js
+++ client/src/components/shared/OrderModal/index.js
@@ -132,13 +132,15 @@ function OrderButtons(props) {
                 <div className='flex flex-row'>
                     <span className="flex-1 text-center">Quantity</span>
                 </div>
-                <div className='flex flex-row justify-center'>
-                    <input type="email" className="form-input mt-1 w-1/2" value='1'/>
-                    <button className='bg-blue-500 rounded-full h-16 w-16 flex items-center justify-center ml-6'>+</button>
-                    <button className='bg-blue-500 rounded-full h-16 w-16 flex items-center justify-center ml-6'>-</button>
+                <div className='flex flex-row'>
+                    <div className='flex-1 inline-flex items-center justify-center my-2'>
+                        <input type="email" className="form-input w-1/2" value='1'/>
+                        <button className='bg-blue-500 rounded-full h-16 w-16 flex items-center justify-center ml-6'>+</button>
+                        <button className='bg-blue-500 rounded-full h-16 w-16 flex items-center justify-center ml-6'>-</button>
+                    </div>
                 </div>
                 <div className='flex flex-row justify-center'>
-                    <button className='bg-red-700 block rounded-full py-2 px-4'>Add to Order</button>
+                    <button className='flex-1 bg-red-700 block rounded-full py-2 px-4'>Add to Order</button>
                 </div>
             </div>            
         </div>
