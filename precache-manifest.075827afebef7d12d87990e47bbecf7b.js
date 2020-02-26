a68affff6fcc71fb13e3ef470d8cf52bbdb88dc6
Thomas Mylott <mylottiv@gmail.com>
2020-02-26 12:43:56 -0600

    Updates

---
+++ precache-manifest.075827afebef7d12d87990e47bbecf7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54edd5bba91190365a3ff7adc1c14ca",
+    "url": "/pizza-ordering-app/index.html"
+  },
+  {
+    "revision": "19b0cf6e2c61f211b0a9",
+    "url": "/pizza-ordering-app/static/css/main.615f8512.chunk.css"
+  },
+  {
+    "revision": "88dda4daedc51e21e4eb",
+    "url": "/pizza-ordering-app/static/js/2.1b91dc51.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/pizza-ordering-app/static/js/2.1b91dc51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19b0cf6e2c61f211b0a9",
+    "url": "/pizza-ordering-app/static/js/main.5cee8714.chunk.js"
+  },
+  {
+    "revision": "66512ab844e883d8deed",
+    "url": "/pizza-ordering-app/static/js/runtime-main.934be5d9.js"
+  }
+]);
